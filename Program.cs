da12a7dcf4b1534d18fd1b485ff628d819213e3f
Shaun Fulton <fulton_shaun@yahoo.com>
2025-04-12 06:38:56 -0400

    User input with null check

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Choose Rock, Paper, or Scissors: "); 
+string userChoice = Console.ReadLine() ?? "";   
