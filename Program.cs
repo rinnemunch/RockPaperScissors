b8a2c903d9b5eecac47c8b3b7709ad4082f29ac0
Shaun Fulton <fulton_shaun@yahoo.com>
2025-04-12 06:48:56 -0400

    compare choices

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
-﻿Console.Write("Choose Rock, Paper, or Scissors: "); 
-string userChoice = Console.ReadLine() ?? "";   
+﻿Console.Write("Choose Rock, Paper, or Scissors: ");
+string userChoice = Console.ReadLine() ?? "";
 
-string[] options = { "Rock", "Paper", "Scissors" }; 
-Random rand = new Random(); 
-string computerChoice = options[rand.Next(options.Length)]; 
+string[] options = { "Rock", "Paper", "Scissors" };
+Random rand = new Random();
+string computerChoice = options[rand.Next(options.Length)];
+
+if (userChoice == computerChoice)
+{
+    Console.Write("It's a tie!");
+}
+else if (
+    (userChoice == "Rock" && computerChoice == "Scissors") ||
+    (userChoice == "Paper" && computerChoice == "Rock") ||
+    (userChoice == "Scissors" && computerChoice == "Paper")
+    )
+{
+    Console.Write("You win!");
+}
+else
+{
+    Console.Write("You lose!");
+}
