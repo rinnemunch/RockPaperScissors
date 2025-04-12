9e5fe9692d535dd3ba0e41e9b2e1ea63f3af3069
Shaun Fulton <fulton_shaun@yahoo.com>
2025-04-12 06:44:24 -0400

    Computer choice

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿Console.Write("Choose Rock, Paper, or Scissors: "); 
 string userChoice = Console.ReadLine() ?? "";   
+
+string[] options = { "Rock", "Paper", "Scissors" }; 
+Random rand = new Random(); 
+string computerChoice = options[rand.Next(options.Length)]; 
